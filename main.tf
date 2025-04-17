5df0ddc705ef689815079be5739c596a10afaa4e
AASanusi <alaminm.sanusi@gmail.com>
2025-04-17 12:09:26 +0100

    Add comment

--- main.tf
+++ main.tf
@@ -6,6 +6,7 @@ resource "aws_s3_bucket" "first-scan-bucket" {
   bucket = "first-scan-bucket"
 }
 
+# Creating s3 backend and copying bucket and state to new backend 
 terraform {
   backend "s3" {
     bucket = "first-scan-bucket"
