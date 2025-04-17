06ca7fc5d266c44310172b7bca6b7258987bf499
AASanusi <alaminm.sanusi@gmail.com>
2025-04-17 11:42:30 +0100

    Change name of bucket to lowercase and hyphens

--- main.tf
+++ main.tf
@@ -2,6 +2,6 @@ provider "aws" {
     region = "eu-west-2" # London Region
 }
 
-resource "aws_s3_bucket" "first_scan_bucket" {
-  bucket = "first_scan_bucket"
+resource "aws_s3_bucket" "first-scan-bucket" {
+  bucket = "first-scan-bucket"
 }
