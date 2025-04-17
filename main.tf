f8053e445f8d54bfd511875ff7da371d42b96cb9
AASanusi <alaminm.sanusi@gmail.com>
2025-04-17 12:03:32 +0100

    Add tfstate and bucket to s3 backend

--- main.tf
+++ main.tf
@@ -5,3 +5,11 @@ provider "aws" {
 resource "aws_s3_bucket" "first-scan-bucket" {
   bucket = "first-scan-bucket"
 }
+
+terraform {
+  backend "s3" {
+    bucket = "first-scan-bucket"
+    key = "global/s3/terraform.tfstate"
+    region = "eu-west-2"
+  }
+}
